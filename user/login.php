44c673fe2541da8821c9ea47ae89184f323c7b19
shubham-ids <shubhamrajput009@gmail.com>
2018-11-01 11:57:21 +0530

    Login file is successfull work

--- user/login.php
+++ user/login.php
@@ -9,15 +9,31 @@
   <!-- /.login-logo -->
   <div class="login-box-body">
     <p class="login-box-msg">Sign in to start your session</p>
-
+    <div>
+      <?php echo $message; ?>
+    </div>
     <form action="" method="post">
       <div class="form-group has-feedback">
-        <input type="email" class="form-control" placeholder="Email">
+        <input type="email" class="form-control" name="email" placeholder="Email">
         <span class="glyphicon glyphicon-envelope form-control-feedback"></span>
+        <div class="serverErrorMessage">
+          <?php
+            if(isset( $emailErrorMessage ) === true){
+              echo $emailErrorMessage;
+            } 
+          ?>
+        </div>         
       </div>
       <div class="form-group has-feedback">
-        <input type="password" class="form-control" placeholder="Password">
+        <input type="password" class="form-control" name="password" placeholder="Password">
         <span class="glyphicon glyphicon-lock form-control-feedback"></span>
+        <div class="serverErrorMessage">
+          <?php
+            if(isset( $passwordErrorMessage ) === true){
+              echo $passwordErrorMessage;
+            } 
+          ?>
+        </div>         
       </div>
       <div class="row">
         <div class="col-xs-8">
@@ -45,7 +61,7 @@
     <!-- /.social-auth-links -->
 
     <a href="#">I forgot my password</a><br>
-    <a href="register.html" class="text-center">Register a new membership</a>
+    <a href="register.php" class="text-center">Register a new membership</a>
 
   </div>
   <!-- /.login-box-body -->
--- user/phpCode/login.php
+++ user/phpCode/login.php
@@ -1,13 +1,48 @@
 <?php
 include('../db/connection.php');
-if(isset($_REQUEST['login'])){
-  $email    = $_REQUEST['email'];
-  $password = $_REQUEST['password'];
-  
-  $validationErrorMssage = false;
-  if(empty($email)){
-    $emailErrorMessage = "This field is required";
-    $validationErrorMssage = true;
+$message = "";
+try{
+  if(isset($_REQUEST['login'])){
+    $email    = $_REQUEST['email'];
+    $password = $_REQUEST['password'];
+    
+    $validationErrorMssage = false;
+    if(empty($email)){
+      $emailErrorMessage = "This field is required";
+      $validationErrorMssage = true;
+    }
+    if(empty($password)){
+      $passwordErrorMessage = "This field is required";
+      $validationErrorMssage = true;
+    }
+    if($validationErrorMssage == false){
+      $query = "
+        SELECT 
+        * 
+        FROM 
+          ".USER." 
+          WHERE 
+          email    = :email 
+        AND
+          password = :password
+          ";
+      $selectQuery = $pdo->prepare($query);
+      $responce    = $selectQuery->execute( [
+                      'email'    => $email , 
+                      'password' => md5($password)
+                    ] );
+      $row = $selectQuery->fetch();
+      $rowCount = $selectQuery->rowCount();
+      if($rowCount > 0){
+        $message = "Wellcome";
+      }else{
+        $message  = "Invalide email and password";
+      }
+    
+    }  
   }
-}
+}catch(PDOException $e){
+    $message = "your record is not insert please contact the developer";
+    //echo $e->getMessage();
+  } 
 ?>
